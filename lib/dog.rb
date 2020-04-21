cbb7f9fa753b8e2da5d42fbb7f1d743ac8fc83ac
carolineglass <carolineglass89@gmail.com>
2020-04-21 16:23:41 -0400

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,33 @@
+class Dog 
+    attr_accessor :name
+    attr_accessor :breed
+#    def name=(name)
+#     @name = name 
+#    end
+#    def name
+#     @name
+#    end
+#    def breed=(breed)
+#     @breed = breed
+#    end
+#    def breed
+#     @breed
+#    end
+end 
+
+class Person 
+    attr_accessor :name
+    attr_accessor :job
+    # def name=(name)
+    #     @name = name 
+    # end 
+    # def name
+    #     @name 
+    # end
+    # def job=(job)
+    #     @job = job
+    # end
+    # def job
+    #     @job
+    # end
+end
